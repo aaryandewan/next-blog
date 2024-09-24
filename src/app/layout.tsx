fdfcfa2a04891f57c49c7f0ccbd065194b70190c
Aaryan Dewan <aaryan.dewan@iongroup.com>
2024-09-24 13:04:06 +0530

    SSG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
           // enableSystem
           // disableTransitionOnChange
         >
-          <main>{children}</main>
+          <main className="flex-1">{children}</main>
           <Footer />
         </ThemeProvider>
       </body>
